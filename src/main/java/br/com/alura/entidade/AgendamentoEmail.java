df7f0888dfcb0e9bd93fbfa29a2c0a4941844f35
Breno Yuri <byurifb@gmail.com>
2023-03-31 00:00:18 -0300

    Implementando Fila para Mensagens
    ...

--- src/main/java/br/com/alura/entidade/AgendamentoEmail.java
+++ src/main/java/br/com/alura/entidade/AgendamentoEmail.java
@@ -2,6 +2,8 @@ package br.com.alura.entidade;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -14,7 +16,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(of = "id")
-public class AgendamentoEmail {
+public class AgendamentoEmail implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = IDENTITY)
